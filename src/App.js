4130a68e7e29aec167312eb11403bc321783669b
Cassiano Oliveira <cassiano980@gmail.com>
2021-11-24 23:22:06 -0300

    Adicionando Cabeçalho

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { AppContainer } from './components/AppContainer'
+import {Home} from "./components/Home/Home"
 
 
 
 
 function App() {
 	return (
-        <AppContainer />
+		<Home />
 	)
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import "./Header.css"
+import "./Body.css"
+
+export class Home extends Component{
+
+
+
+    render(){
+        return (
+            <div>
+              <div className="header" >
+                <h1> LabeNinjas </h1>
+                <button className="buttonHome">Home</button>
+                <button className="buttonCarrinho">Carrinho</button>
+              </div>
+              <div>
+                  <h1> LabeNinjas </h1>
+                  <h3> O talento certo no momento certo</h3>
+              </div>
+            </div>
+        )
+    }
+}
+
