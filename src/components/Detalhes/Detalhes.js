67e54f97d1e4cf9075677f57f91111d8f985b189
leozartino <leozartino@gmail.com>
2021-11-29 17:05:23 -0300

    bugfix add  correcoes detalhes pagina

--- src/components/Detalhes/Detalhes.js
+++ src/components/Detalhes/Detalhes.js
@@ -1,28 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
-import { DataContext } from "../Context/Context";
-
+import { ContainerDetalhes, DetalhesContainer } from "./Detalhes.styled";
+import axios from "axios";
+import serviceImg from "../../img/service_img.jpg";
+import { Link } from "react-router-dom";
 export default function Detalhes() {
-  /*   state = {
-    jobs: [],
-  }; */
-
-  /*   getJob = () => {
-    if (this.props.match.params.id) {
-      const res = this.context.jobs;
-      const data = res.filter((job) => {
-        return job.id === this.props.match.params.id;
-      });
-
-      console.log(data);
-    }
-  };
-
-  componentDidMount() {
-    this.getJob();
-  } */
-
   const { id } = useParams();
+  const [jobItem, setJob] = useState({
+    id: "",
+    title: "",
+    description: "",
+    price: 0,
+    paymentMethods: ["", "", ""],
+    dueDate: "",
+    taken: false,
+  });
 
-  return <div>Detalhes {id}</div>;
+  useEffect(() => {
+    axios
+      .get(`https://labeninjas.herokuapp.com/jobs/${id}`, {
+        headers: {
+          Authorization: "5ad4040c-731a-403d-9583-f511e3de1a8a",
+        },
+      })
+      .then((response) => {
+        setJob(response.data);
+      });
+  }, [id, jobItem]);
+
+  return (
+    <ContainerDetalhes>
+      <DetalhesContainer>
+        <img src={serviceImg} alt="serviço" />
+        <div className="box">
+          <div className="row">
+            <h2>{jobItem.title}</h2>
+            <span>R$ {jobItem.price}</span>
+          </div>
+          <p>{jobItem.description}</p>
+          <p>{jobItem.dueDate}</p>
+          <Link to="/cart" className="cart">
+            Adicionar ao carrinho
+          </Link>
+        </div>
+      </DetalhesContainer>
+    </ContainerDetalhes>
+  );
 }
