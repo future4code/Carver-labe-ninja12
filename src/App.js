116a3dec6d91fce80983b02964b016467893e371
leozartino <leozartino@gmail.com>
2021-11-29 10:26:50 -0300

    feat: add estilos globais e roteamento no App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
-/* import axios from "axios";
-import styled from "styled-components"; */
+/* 
 /* import CadastroServicos from "./components/CadastroServicos/CadastroServicos"; */
 // import ContratarServico from "./pages/ContratarServico/ContratarServico";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AppContainer } from "./App.styled";
+import { DataProvider } from "./components/Context/Context";
 import Header from "./components/Header/Header";
 import Section from "./components/Section/Section";
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppContainer>
-        <Router>
-          <Header />
-          <Section />
-        </Router>
-      </AppContainer>
+      <DataProvider>
+        <div className="app">
+          <Router>
+            <Header />
+            <Section />
+          </Router>
+        </div>
+      </DataProvider>
     );
   }
 }
