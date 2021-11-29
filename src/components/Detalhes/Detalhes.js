8eabebb6b6f8ce2a18339046227e35159b9e2e16
leozartino <leozartino@gmail.com>
2021-11-29 14:10:26 -0300

    feat: add pagina de detalhes initial

--- src/components/Detalhes/Detalhes.js
+++ src/components/Detalhes/Detalhes.js
@@ -1,7 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
+import { useParams } from "react-router";
+import { DataContext } from "../Context/Context";
 
-export default class Detalhes extends Component {
-  render() {
-    return <div>Detalhes</div>;
-  }
+export default function Detalhes() {
+  /*   state = {
+    jobs: [],
+  }; */
+
+  /*   getJob = () => {
+    if (this.props.match.params.id) {
+      const res = this.context.jobs;
+      const data = res.filter((job) => {
+        return job.id === this.props.match.params.id;
+      });
+
+      console.log(data);
+    }
+  };
+
+  componentDidMount() {
+    this.getJob();
+  } */
+
+  const { id } = useParams();
+
+  return <div>Detalhes {id}</div>;
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,13 +7,13 @@ export default class Footer extends Component {
       <FooterContainer>
         <div className="midias-sociais">
           <a href="https://www.facebook.com/" target="_blank">
-            <i class="fab fa-facebook-f"></i>
+            <i className="fab fa-facebook-f"></i>
           </a>
           <a href="https://twitter.com/home" target="_blank">
-            <i class="fab fa-twitter"></i>
+            <i className="fab fa-twitter"></i>
           </a>
           <a href="https://www.instagram.com/" target="_blank">
-            <i class="fab fa-instagram"></i>
+            <i className="fab fa-instagram"></i>
           </a>
           <p>© 2021 Labeninjas S.A</p>
         </div>
